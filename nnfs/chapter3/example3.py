aa0c6bac241a4c3920ade746fca87eed9d60b123
Hob-77 <hobexe77@gmail.com>
2026-03-19 01:51:50 -0500

    Finished chapter 3

---
+++ nnfs/chapter3/example3.py
@@ -0,0 +1,18 @@
+import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+import denselayer
+
+nnfs.init()
+
+# Create dataset
+X, y = spiral_data(samples=100, classes=3)
+
+# Create Dense layer with 2 input features and 3 output values
+dense1 = denselayer.Layer_Dense(2,3)
+
+# Perform a forward pass of our training data through this layer
+dense1.forward(X)
+
+# Let's see output of the first few samples:
+print(dense1.output[:5])
